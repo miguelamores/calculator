4468805f0e3ef1b294db0a27d13787b33d5d433c
miguelamores <miguel-amores@miguel.trade.com.ec>
2019-03-26 17:58:41 -0500

    set value of slider

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const slider = document.getElementById("myRange");
     let output = document.getElementById("year");
-    output.innerHTML = slider.value; // Display the default slider value
+    output.value = slider.value; // Display the default slider value
     
     // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function() {
-        output.innerHTML = this.value;
+        output.value = this.value;
     }
 
 });
